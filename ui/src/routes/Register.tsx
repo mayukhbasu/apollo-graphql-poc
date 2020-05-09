faad4b6f39e707a82d4fb2fc4ca737d601751092
Mayukh Basu <roxy.firebird@gmail.com>
2020-05-09 18:42:49 +0530

    starting to use hooks in all react instances

--- ui/src/routes/Register.tsx
+++ ui/src/routes/Register.tsx
@@ -1,76 +1,11 @@
-import React, {Component} from 'react';
-import { Button, FormGroup, Label, Input,Container, Row, Col, Form } from 'reactstrap';
+import React, { PureComponent } from 'react'
 
-class Register extends Component{
-    state = {
-        email: "",
-        password: "",
-        confirmPassword: ""
-    }
-
-    setValue = (event:any) => {
-        event.persist()
-        console.log(event.target.id);
-        if(event.target.id === 'exampleEmail') {
-            this.setState({
-                email: event.target.value
-            })
-        }
-
-        if(event.target.id === 'examplePassword') {
-            this.setState({
-                password: event.target.value
-            })
-        }
-
-        if(event.target.id === 'cpassword') {
-            this.setState({
-                confirmPassword: event.target.value
-            })
-        }
-    }
-
-    submit = () => {
-        console.log(this.state);
-    }
-    render(){
-        return (
-            <Container className="mt-4">
-            <Form>
-                <Row>
-                <Col xs="6">
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail"
-                    onChange={this.setValue}/>
-                </FormGroup>
-                </Col>
-                </Row>
-                <Row>
-                <Col xs="6">
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" 
-                    onChange={this.setValue}/>
-                </FormGroup>
-                </Col>
-                </Row>
-                <Row>
-                <Col xs="6">
-                <FormGroup>
-                    <Label for="cpassword">Confirm Password</Label>
-                    <Input type="password" name="cpassword" id="cpassword"
-                    onChange={this.setValue}/>
-                </FormGroup>
-                </Col>
-                </Row>
-                <Button color="primary" size="sm" onClick={this.submit}>Submit</Button>{' '}
-                <Button color="warning" size="sm">Reset</Button>{' '}
-                </Form>
-            </Container>
-            
-        )
-    }
+const Register = (props:any) => {
+    return (
+        <div>
+            Register Component works
+        </div>
+    )
 }
 
 export default Register;
